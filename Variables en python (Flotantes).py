f378199c63b422bbae30c2b190c35bd071b4f2a7
Hazel <sotovargasdiegoo@gmail.com>
2025-02-05 18:55:42 -0600

    Variables en python (Flotantes).py

---
+++ Variables en python (Flotantes).py
@@ -0,0 +1,9 @@
+#VARIABLES EN PYTHON
+
+#VARIABLES EN PYTHON (FLOTANTES)
+
+variable1= 10.5
+variable2= 20.7     #SON AQUELLOS QUE TIENEN RESIDUO
+variable3= 30.10
+
+print(variable1 + variable2 + variable3)

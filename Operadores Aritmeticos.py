640f95c4833912b9da48ada2c3e4102fa4d3c773
Hazel <sotovargasdiegoo@gmail.com>
2025-02-06 18:16:58 -0600

    Operadores Aritmeticos.py

---
+++ Operadores Aritmeticos.py
@@ -0,0 +1,52 @@
+                
+                
+                     #OPERACIONES ARITMETICAS
+
+#SUMA
+A = 2
+B = 2
+
+#OPERACIONES
+
+print(A + B)
+
+
+
+ 
+#RESTA
+B = 3
+C = 2
+
+#OPERACIONES
+print(B - C)
+
+
+
+
+#MULTIPLICACION
+D = 3
+E = 3
+
+#OPERACIONES
+print(D * E)
+
+
+
+
+# DIVISION
+F = 5
+G = 2.5
+
+#OPERACIONES
+
+print(E / G)
+
+
+#MODULO 
+
+H = 10
+I = 3
+
+#OPERACIONES
+
+print( H % I)

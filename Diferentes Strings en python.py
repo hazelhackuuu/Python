b0f1e2a5bc5fb94f59c8562472b9f7d70a92cbc2
Hazel <sotovargasdiegoo@gmail.com>
2025-02-06 18:05:47 -0600

    Diferentes Strings en python.py

---
+++ Diferentes Strings en python.py
@@ -0,0 +1,34 @@
+     #STRINGS EN PYTHON
+
+#STRINGS EN PYTHON
+
+variables= "prueba uno"
+
+variables1= "PRUEBA DOS"
+
+variables2= "Prueba Tres"
+
+variables3= "Prueba Cuatro"
+
+variables4= "Prueba Cinco"
+
+
+#EJEMPLOS DE USO
+
+
+print(variables.upper)  #HACE QUÉ LA VARIABLE SE PONGA EN MAYUSCULAS.
+
+print(variables1.lower)  #HACE QUÉ LA VARIABLE SE PONGA EN MINUSCULAS.
+
+print(variables2.find("u")) #HACE QUE LA VARIABLE UBIQUE LA LETRA.
+
+print(variables3.replace("Hola", "Holaa")) #SE SELECCIONA UN DATO DE LA VARIABLE Y SE CAMBIA POR EL DESEADO.
+
+print(variables4 in "Prueba") #SE SELECCIONA UN DATO Y SE LE PIDE UBICARLO Y SI ES ASI IMPRIME SI ES "TRUE" O "FALSE".
+
+
+
+
+
+
+

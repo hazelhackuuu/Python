93ea541b206889e391fe2f3cbac95f3cb62ba1e2
Hazel <sotovargasdiegoo@gmail.com>
2025-02-09 20:59:01 -0600

    Converesion de Datos.py

---
+++ Converesion de Datos.py
@@ -0,0 +1,31 @@
+#CONVERSION DE DATOS EN PYTHON
+
+
+#EJEMPLO NUMERO 1
+
+ejemplonumero1= input("Ingresa un numero:")
+print(type(ejemplonumero1))        #EJEMPLO DE SUMA DE DATOS
+numero = int(ejemplonumero1)
+print(numero + 2)
+
+
+
+
+
+#EJEMPLO  NUMERO 2
+
+ejemplonumero2 = input("Ingrea un numero:")
+print(type(ejemplonumero2))
+numero1= int(ejemplonumero2)     #EJEMPLO DE RESTA DE DATOS
+print(numero1 - 2 )
+
+
+
+
+
+#EJEMPLO NUMERO 3
+
+ejemplonumero3 =  input("Ingrese un numero:")
+print(type(ejemplonumero3))
+numero2= int(ejemplonumero3)    #EJEMPLO DE MULTIPLICACION DE DATOS
+print(ejemplonumero3 * 2)

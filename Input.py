f0207a4cf043cb9f7ac557a8905d1f6420a78e29
Hazel <sotovargasdiegoo@gmail.com>
2025-02-09 20:27:43 -0600

    Input.py

---
+++ Input.py
@@ -0,0 +1,52 @@
+                                             #PEQUEÑO LOGIN DE DATOS DEL USUARIO
+
+
+
+#INPUT EN PYTHON
+
+
+
+#PEQUEÑO LOGGEO 
+
+#PRIMERA PARTE   #///PEDIR EL NOMBREC DEL INDIVIDUO
+
+print("Ingresa tu nombre completo:")
+
+nombre = input(("Nombre Completo:"))
+
+print(nombre)
+
+
+
+#PRIMERA PARTE
+
+print("INGRESA TUS APELLIDOS:")
+
+apellidos = input(("Apellidos Completos:"))
+
+print(apellidos)                   
+                      
+                      
+                      
+#SEGUNDA PARTE
+
+print("INGRESA TU EDAD")
+
+edad = input(("Ingresa tu edad:"))
+
+print(edad)
+
+
+
+
+
+#TERCERA PARTE
+
+print("INGRESA TU DOMICILIO")
+
+domicilio = input(("Ingresa tu domicilio:"))
+
+print(domicilio)
+
+
+

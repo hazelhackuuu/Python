710eb04bb7094d507dcd34c64ce4df29a71ca362
Hazel <sotovargasdiegoo@gmail.com>
2025-02-11 16:54:19 -0600

    Valores Logicos (if. else. elif).py

---
+++ Valores Logicos (if. else. elif).py
@@ -0,0 +1,23 @@
+#VALORES LOGICOS PYTHON
+
+#EJEMPLO NUMERO 1
+
+#EJEMPLO NUMERO 1 
+
+#ENTRADA DE DATOS CON VALORES
+
+puntaje = int(input("Ingresa tu puntaje: "))
+
+if puntaje >= 100:
+    
+    print("Aprobado")
+    
+elif puntaje <= 0:
+    
+    print("Reprobado")
+    
+else:
+    print("Reprobado")
+    
+2
+    

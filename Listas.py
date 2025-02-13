a0bb8375f7cff0843e2eedd839acb15a1d17950a
Hazel <sotovargasdiegoo@gmail.com>
2025-02-13 16:48:39 -0600

    Listas.py

---
+++ Listas.py
@@ -0,0 +1,52 @@
+#LISTAS EN PYTHON 
+
+#EJEMPLO NUMERO 1
+
+#              0       1     2         3         4    5    6     7
+lenguajes0= ["Python , Ruby, Java,  Javascript, Lua ,C# , C+, C++"]
+
+print(lenguajes0)            #IMPRIMIR TODA LA LISTA DE VALORES
+
+
+
+#EJEMPLO NUMERO 2 (IMPRIMRIR DATOS DE UN VALOR)
+
+#              0          1       2         3          4     5      6     7
+lenguajes = ["Python", "Ruby", "Java", "Javascript", "Lua", "C#", "C+", "C++"]
+print(lenguajes[2])
+  
+                             #ESTE ES PARA UBICAR UN VALOR Y IMPRIMIRLO.
+
+#EJEMPLO NUMERO 4 
+
+#              0          1       2         3          4     5      6     7
+lenguajes1 = ["Python", "Ruby", "Java", "Javascript", "Lua", "C#", "C+", "C++"]
+print(lenguajes1)
+lenguajes1[1] = "HTML"
+print(lenguajes1)
+ 
+                          #ESTE ES PARA UBICAR UN VALOR Y REMPLAZARLO.
+                          
+                          
+#EJEMPLO NUMERO 5
+
+#              0          1       2         3          4     5      6     7
+lenguajes2 = ["Python", "Ruby", "Java", "Javascript", "Lua", "C#", "C+", "C++"]
+lenguajes2[1] = "GOGOGO"
+print(lenguajes2[-1])
+
+
+                        #IMPRIMIR UN VALOR, EN ESTE CASO EL ULTIMO VALOR.
+                        
+#EJEMPLO NUMERO 6
+
+#              0          1       2         3          4     5      6     7
+lenguajes3 = ["Python", "Ruby", "Java", "Javascript", "Lua", "C#", "C+", "C++"]
+lenguajes3[1] = "HTML"
+print(lenguajes3[0:4])
+ 
+ 
+                        #IMPRIMIR UNO LISTA DE OBJETOS DESDE DONDE LE INDIQUEMOS.
+                        
+                        
+                        

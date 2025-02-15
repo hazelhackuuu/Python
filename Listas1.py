eddde3e80f9c6d72050a17798d2f1f29393c35fc
Hazel <sotovargasdiegoo@gmail.com>
2025-02-14 21:22:43 -0600

    Listas1.py

---
+++ Listas1.py
@@ -0,0 +1,57 @@
+#LISTAS EN PYTHON
+
+
+#EJEMPLO NUMERO 1
+
+#              0          1        2          3          4    
+lenguajes0= ["Python" , "Ruby", "Java",  "Javascript", "Lua"]
+
+lenguajes0.insert( 3, "HTML")
+
+print(lenguajes0)       #Modificar un valor de la lista y adjuntar uno nuevo.
+
+
+#EJEMPLO NUMERO 2
+
+
+#              0          1        2          3          4    
+lenguajes1= ["Python" , "Ruby", "Java",  "Javascript", "Lua"]
+
+lenguajes1.remove("Java")
+
+print(lenguajes1)            #Remover un valor de la lista.
+
+
+#EJEMPLO NUMERO 3
+
+#              0          1        2          3          4    
+lenguajes2= ["Python" , "Ruby", "Java",  "Javascript", "Lua"]
+
+print( "C"  in lenguajes2)    #Consultar si esta disponible el valor en la lista.
+
+
+#EJEMPLO NUMERO 4
+
+#              0          1        2          3          4    
+lenguajes3= ["Python" , "Ruby", "Java",  "Javascript", "Lua"]
+
+lenguajes3.clear()       #Este sirve para limpiar  la lista.
+
+#EJEMPLO NUMERO 5
+
+#              0          1        2          3          4    
+lenguajes4= ["Python" , "Ruby", "Java",  "Javascript", "Lua"]
+
+print (len(lenguajes4))
+
+
+A
+
+
+
+
+
+
+
+
+

02a206c7c33d5ecfbae4d9a6b63061b52ae2c7c0
Hazel <sotovargasdiegoo@gmail.com>
2025-02-16 22:52:51 -0600

    Area del rectangulo (Proyecto).py

---
+++ Area del rectangulo (Proyecto).py
@@ -0,0 +1,38 @@
+                 #DETERMINAR EL AREA DE UN RECTANGULO 
+
+                         #PARTE NUMERO 1
+                       #Diego Soto Vargas
+                       
+                       
+                       
+                       
+                       
+#Solicitar al usario los datos, base y altura,
+
+#Creacion de variables y datos flotanes, el motivo puede tener residuo al ser en cm.
+
+basedelrectangulo= float(input("Introduce la base del rectangulo:"))
+
+
+alturadelrectangulo= float(input("Introduce la altura del triangulo:"))
+
+
+
+
+
+#Calcular el áraa del triangulo 
+
+area= basedelrectangulo * alturadelrectangulo
+
+#Se crea la variable (area) y se adjunta la base x altura.
+
+
+
+
+
+#Mostrar el resultado.
+
+
+print("El area del triangulo es:" + str(area))
+
+#Se procede a imprimir el area del triangulo, y se le concatena el area con un string.
